614bd7a83b24e313ef9950aaf4bc832220ec5e1a
Bruna <brunaahm@gmail.com>
2025-11-20 18:21:52 -0300

    fix: limpando a estrutura do projeto

--- dbModels.js
+++ dbModels.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'fatec',
-    database: 'portfolio'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = {
